72d30d91e3b82839f1e50e9c394b796a87665be5
mehdi <mbdj@free.fr>
2021-07-05 13:17:48 +0200

    prise en compte du nombre de pales pour le calcul de vitesse

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,13 @@
 const uint8_t pinIN{13};             // pin de lecture du capteur : HIGH si capte un objet réfléchissant à proximité sinon LOW
 unsigned long changeStateCounter{0}; // compte le nombre de changements d'état du capteur afin de calculer la vitesse du moteur
 
-unsigned long oldTime;                        // instant du dernier changement de valeur
-unsigned long currentTime;                    // instant de le mesure courante
-const unsigned long delayMs{1000};            // délai de mesure en milliseconde
-const unsigned long delaySec{delayMs / 1000}; // delayMs précédent ramené en sec pour optimiser le calcul de vitesse
+const int numberOfBlades{4}; // nombre de pales du moteur
+                             // pour chaque pale on a 2 changements du capteur
 
+unsigned long beginTime;                                  // instant du dernier changement de valeur
+unsigned long currentTime;                                // instant de le mesure courante
+const unsigned long delayMs{1000};                        // délai de mesure en milliseconde
+const unsigned long k{delayMs / (numberOfBlades * 1000)}; // coefficient par lequel il faut multiplier le nombre de changements pour avoir la vitesse en tr/sec
 int newValue;
 int oldValue;
 
@@ -17,15 +19,20 @@ void setup()
   pinMode(pinIN, INPUT);
   Serial.begin(9600);
 
-  oldTime = micros();
-  oldValue = LOW;
+  beginTime = micros(); // initialisation
+  oldValue = LOW;       // initialisation. L'état est supposé LOW au départ
 }
 
 void loop()
 {
+  // affichage de l'état du capteur : HAUT = détection ; BAS = pas de détection
   //digitalRead(pinIN) == HIGH ? Serial.write("HAUT\n") : Serial.write("BAS\n");
-  currentTime = millis();
 
+  // On incrémente un compteur durant le délai delayMs
+
+  // Si on détecte un changement d'état du capteur on incrémente le compteur
+  // et la nouvelle valeur devient la valeur "ancienne" à comparer
+  currentTime = millis();
   newValue = digitalRead(pinIN);
   if (newValue != oldValue)
   {
@@ -33,11 +40,12 @@ void loop()
     oldValue = newValue;
   }
 
-  unsigned long d = currentTime - oldTime;
+  // quand le delai delayMs est atteint on affiche le résultat et on réinitialise le compteur
+  unsigned long d = currentTime - beginTime; // délai depuis le début du comptage
   if (d > delayMs)
   {
-    Serial.println(changeStateCounter / delaySec);
-    oldTime = currentTime;
+    Serial.println(changeStateCounter * k);
+    beginTime = currentTime;
     changeStateCounter = 0;
   }
 }
